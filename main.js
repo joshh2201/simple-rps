794565f5466c6c12e896cc2aac3b1d3b9c5d8b31
Josh <joshh2201@gmail.com>
2023-04-26 22:09:36 -0400

    Add function to determine winner of one round

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 function getComputerChoice() {
-  let choices = ['rock', 'paper', 'scissors'];
+  let choices = ['Rock', 'Paper', 'Scissors'];
   return choices[Math.floor(Math.random() * 3)];
+}
+
+function capitalize(str) {
+  str = str.toLowerCase();
+  return str[0].toUpperCase() + str.substring(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitalize(playerSelection);
+  if (playerSelection === computerSelection) {
+    return `You Tied! The computer also picked ${computerSelection}`;
+  }
+  let won =
+    (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+    (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+    (playerSelection === 'Scissors' && computerSelection === 'Paper');
+  if (won) {
+    return `You Won! ${playerSelection} beats ${computerSelection}`;
+  }
+  return `You Lose! ${computerSelection} beats ${playerSelection}`;
 }
